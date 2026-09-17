cc5eae4bb37871f46bdbe9393659614203095d61
MSP-byte <mespecial92@gmail.com>
2026-09-16 23:29:31 -0300

    test: actualizar contrato Supabase a circuito v3

--- tests/check_supabase_contract.js
+++ tests/check_supabase_contract.js
@@ -175,7 +175,7 @@ for (const pattern of [
   /client\.rpc\('coi_anular_consumo_posicion'/,
   /client\.rpc\('coi_actualizar_orden_integral'/,
   /client\.rpc\('coi_eliminar_orden_integral'/,
-  /\.rpc\('coi_confirmar_etapa_circuito_v2'/
+  /\.rpc\('coi_confirmar_etapa_circuito_v3'/
 ]) assert.match(html, pattern);
 assert.doesNotMatch(html, /\.rpc\('coi_guardar_link_documental'/);
 assert.doesNotMatch(html, /\.rpc\('coi_eliminar_link_documental'/);
